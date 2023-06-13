d89c8fdbe2c69bc3180b99922f9ea89be6540cc2
DevExpressExampleBot <fakeEmail>
2023-06-13 15:51:15 +0400

    Source auto update [skip ci]

--- VB/WindowsApplication3/Program.vb
+++ VB/WindowsApplication3/Program.vb
@@ -1,19 +1,20 @@
-﻿Imports System
-Imports System.Collections.Generic
+Imports System
 Imports System.Windows.Forms
 Imports DevExpress.Skins
 
 Namespace DXSample
-	Friend Module Program
-		''' <summary>
-		''' The main entry point for the application.
-		''' </summary>
-		<STAThread>
-		Sub Main()
-			SkinManager.EnableFormSkins()
-			Application.EnableVisualStyles()
-			Application.SetCompatibleTextRenderingDefault(False)
-			Application.Run(New Main())
-		End Sub
-	End Module
+
+    Friend Module Program
+
+        ''' <summary>
+        ''' The main entry point for the application.
+        ''' </summary>
+        <STAThread>
+        Sub Main()
+            Call SkinManager.EnableFormSkins()
+            Call Application.EnableVisualStyles()
+            Application.SetCompatibleTextRenderingDefault(False)
+            Call Application.Run(New Main())
+        End Sub
+    End Module
 End Namespace
